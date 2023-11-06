f003d6fd893043caa93e0e2b1a8bdb1d41b446e1
joaomoraiss <joaovictorrmpework@outlook.com>
2023-11-06 19:45:12 -0300

    Classe FAQ feita

--- FAQ.java
+++ FAQ.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+
 public class FAQ {
+    protected ArrayList<String> perguntas;
+    protected ArrayList<String> respostas;
+
+    public FAQ(ArrayList<String>perguntas, ArrayList<String> respostas) {
+      this.perguntas = new ArrayList<String>(perguntas);
+      this.respostas = new ArrayList<String>(respostas);
+    }
+    public void alterarResposta(String novaResposta, String pergunta){
+        for (int i = 0; i < this.perguntas.size(); i++) {
+            if (pergunta.equals(this.perguntas.get(i))) {
+                this.perguntas.set(i, novaResposta);
+            }
+        }
+    }
 }
