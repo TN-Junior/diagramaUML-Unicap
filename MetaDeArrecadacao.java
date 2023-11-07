288a3b3188c98c414de7a6daa6adca1297a2505b
MrzDavi <93688754+MrzDavi@users.noreply.github.com>
2023-11-06 23:54:38 -0300

    Classes

--- MetaDeArrecadacao.java
+++ MetaDeArrecadacao.java
@@ -1,2 +1,27 @@
 public class MetaDeArrecadacao {
+    private double valorMeta;
+    private double valorArrecadado;
+
+    public MetaArrecadacao(double valorMeta) {
+        this.valorMeta = valorMeta;
+        this.valorArrecadado = 0.0;
+    }
+
+    public double getValorMeta() {
+        return valorMeta;
+    }
+
+    public double getValorArrecadado() {
+        return valorArrecadado;
+    }
+
+    public void receberDoacao(double valorDoacao) {
+        valorArrecadado += valorDoacao;
+    }
+
+    public boolean metaAtingida() {
+        return valorArrecadado >= valorMeta;
+    }
+}
+
 }
